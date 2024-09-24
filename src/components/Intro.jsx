5ecbb09b8729424fb5e0ca6c038c76b18d2e33bc
HaDock404 <delescluse.g@gmail.com>
2024-09-24 17:08:57 +0200

    intro text appearance

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,33 @@
 import { Link } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
 import '../styles/intro.css'
 
 function Intro(props) {
+    const boxRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add('show');
+              observer.unobserve(entry.target);
+            }
+          });
+        });
+    
+        // Observer chaque élément de la liste
+        const elements = boxRef.current.querySelectorAll('.appear');
+        elements.forEach(element => observer.observe(element));
+    
+        // Clean-up pour éviter d'observer encore après le démontage
+        return () => {
+          elements.forEach(element => observer.unobserve(element));
+        };
+      }, []);
+
     return (
-        <div class="hero">
-            <h1>
+        <div class="hero" ref={boxRef}>
+            <h1 className='appear'>
                 <span class="highlight">{props.word_start}</span>{props.sentence}<span class="highlight">{props.word_end}</span>.
             </h1>
             <Link to="/" class="cta-button">{props.button}</Link>
