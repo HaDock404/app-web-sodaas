59923b7b56b002ee386f137f3cd7ae62d33618e4
HaDock404 <delescluse.g@gmail.com>
2024-09-24 17:15:03 +0200

    add appearance

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -1,9 +1,34 @@
 import '../styles/ad.css'
+import React, { useEffect, useRef } from 'react';
 
 function Ad() {
-    return (
-        <section className='ad-section'>
+    const boxRef = useRef(null);
 
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add('show');
+              observer.unobserve(entry.target);
+            }
+          });
+        });
+    
+        // Observer chaque élément de la liste
+        const elements = boxRef.current.querySelectorAll('.appear');
+        elements.forEach(element => observer.observe(element));
+    
+        // Clean-up pour éviter d'observer encore après le démontage
+        return () => {
+          elements.forEach(element => observer.unobserve(element));
+        };
+      }, []);
+
+    return (
+        <section className='ad-section' ref={boxRef}>
+            <article className='ad-article appear'>
+
+            </article>
         </section>
     )
 }
