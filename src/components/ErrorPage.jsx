7b1cafc77df4008d571b8df9fd9e722e5568eb56
HaDock404 <delescluse.g@gmail.com>
2024-10-04 22:29:04 +0200

    design and translation

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/errorpage.css';
+import React, { useContext } from 'react';
+import { LanguageContext } from './LanguageContext';
 
-const ErrorPage = ({ statusCode, message }) => {
+function ErrorPage({ statusCode }){
   const navigate = useNavigate();
+  const { language } = useContext(LanguageContext);
+
+  const error = {
+    en: 'Error',
+    pl: 'Błąd'
+  };
+
+  const message = {
+    en: 'The page you are looking for does not exist.',
+    pl: 'Strona, której szukasz nie istnieje.'
+  };
+
+  const back = {
+    en: 'Back to home',
+    pl: 'Powrót do domu'
+  };
 
   const handleBackToHome = () => {
     navigate('/');
@@ -11,10 +28,10 @@ const ErrorPage = ({ statusCode, message }) => {
 
   return (
     <div className="error-page">
-      <h1>Erreur {statusCode || '404'}</h1>
-      <p>{message || "La page que vous cherchez n'existe pas."}</p>
-      <button onClick={handleBackToHome} className="back-button">
-        Retour à l'accueil
+      <h1 className='error-message'>{error[language]} {statusCode || '404'}</h1>
+      <p>{message[language]}</p>
+      <button onClick={handleBackToHome} className="back-button cta-button">
+        {back[language]}
       </button>
     </div>
   );
